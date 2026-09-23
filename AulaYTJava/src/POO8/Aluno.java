0f7690e73cd755d7f67a722779ab6d0e84adbcb5
Daniell Cardoso <daniellocs25@gmail.com>
2026-09-23 15:25:12 -0300

    Alteracoes

--- AulaYTJava/src/POO8/Aluno.java
+++ AulaYTJava/src/POO8/Aluno.java
@@ -11,23 +11,28 @@ public class Aluno {
     private double inteligencia;
     private double lealdade;
     private double ambicao;
-    private String casa;
+    private double estrategia;
+    private double criatividade;
+    
+
 
     //CONSTRUTOR
-    public Aluno(String nome, int idade, double coragem, double inteligencia, double lealdade, double ambicao, String casa){
+    public Aluno(String nome, int idade, double coragem, double inteligencia, double lealdade, double ambicao, double estrategia, double criatividade,String casa){
         this.nome = nome;
         this.idade = idade;
         this.coragem = coragem;
         this.inteligencia= inteligencia;
         this.lealdade = lealdade;
         this.ambicao = ambicao;
-        this.casa = casa;
+        this.estrategia = estrategia;
+        this.criatividade = criatividade;
+        
     }
 
     //METODOS
     public void exibirInformacoes(){
-        System.out.println(nome);
 
+        System.out.println(nome);
         System.out.println(idade);
         System.out.println(coragem);
         System.out.println(inteligencia);
@@ -40,10 +45,10 @@ public class Aluno {
     public double grifinoria(){
         return ((2 * coragem) + lealdade);
     }
-    public double sonserina(double ambicao, double estrategia){
+    public double sonserina(){
         return ((2 * ambicao) + estrategia);
     }
-    public double corvinal(double criatividade){
+    public double corvinal(){
         return ((2 * inteligencia) + criatividade);
 
     }
@@ -51,8 +56,21 @@ public class Aluno {
         return (((2 * lealdade) + coragem) / 3);
     }
 
-    public String calcularCasa(){
-        if ()
+    public void calcularCasa(Aluno aluno){
+        String casa;
+        if (aluno.grifinoria() > aluno.sonserina() && aluno.grifinoria() > aluno.corvinal() && aluno.grifinoria() > aluno.lufalufa()) {
+            casa = "Grifinoria";
+        }
+        if (aluno.sonserina() > aluno.grifinoria() && aluno.sonserina() > aluno.corvinal() && aluno.sonserina() > aluno.lufalufa()){
+            casa = "Sonserina";
+        }
+        if (aluno.corvinal() > aluno.grifinoria() && aluno.corvinal() > aluno.sonserina() && aluno.corvinal() > aluno.lufalufa()){
+            casa = "Corvinal";
+        }
+        if (aluno.lufalufa() > aluno.grifinoria() && aluno.lufalufa() > aluno.sonserina() && aluno.lufalufa() > aluno.corvinal()){
+            casa = "Lufa-Lufa";
+        }
+ }
 
     }
-}
+
--- AulaYTJava/src/POO8/Main.java
+++ AulaYTJava/src/POO8/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     static void main(String[] args) {
 
-        Aluno aluno1 = new Aluno();
-
-
+        Aluno aluno1 = new Aluno("Daniell", 19, 6.7, 4.0, 7.9, 8.1, 7.6, 9.0);
+        
+        
     }
 }
